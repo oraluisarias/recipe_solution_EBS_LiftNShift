968f44b778cdb9c185c5502ca22ddab31c40fba3
luisaria <luisaria>
2017-05-15 10:31:18 -0500

    demo central class

--- post_creation.sh
+++ post_creation.sh
@@ -2,17 +2,19 @@
 gse_admin="gse-admin.oraclecloud.com"
 identity_domain=$1
 target_ip=$2
+gse_admin_stagedir="/home/opc/EBS_LiftNShift/"${identity_domain}
 
 [ ! -f scripts/p22336899_R12_GENERIC.zip ] && cat scripts/p22336899_R12_GENERIC.zip* > scripts/p22336899_R12_GENERIC.zip
 echo "Deleting existing ssh keys and authorized cache..."
-ssh -o StrictHostKeyChecking=no -i ssh_keys/gse_admin opc@${gse_admin} 'rm -rf /home/opc/.ssh/known_hosts ~/'${identity_domain}
+ssh -o StrictHostKeyChecking=no -i ssh_keys/gse_admin opc@${gse_admin} 'mkdir -p '${gse_admin_stagedir}' && rm -rf /home/opc/.ssh/known_hosts '${gse_admin_stagedir}'/'${identity_domain}
 echo "Uploading assets to gse admin..."
-scp -o StrictHostKeyChecking=no -i ssh_keys/gse_admin ssh_keys/${identity_domain} scripts/root_ebs_workshop.sh scripts/oracle_ebs_workshop.sh scripts/p22336899_R12_GENERIC.zip opc@${gse_admin}:~/
+scp -o StrictHostKeyChecking=no -i ssh_keys/gse_admin ssh_keys/${identity_domain} scripts/root_ebs_workshop.sh scripts/oracle_ebs_workshop.sh scripts/p22336899_R12_GENERIC.zip opc@${gse_admin}:${gse_admin_stagedir}
 echo "Preparing ssh key to connect to target instance (${target_ip})..."
-ssh -o StrictHostKeyChecking=no -i ssh_keys/gse_admin opc@${gse_admin} 'chmod 0400 ~/'${identity_domain}
+ssh -o StrictHostKeyChecking=no -i ssh_keys/gse_admin opc@${gse_admin} 'chmod 0400 '${gse_admin_stagedir}'/'${identity_domain}
 echo "Uploading assets to target instance..."
-ssh -o StrictHostKeyChecking=no -i ssh_keys/gse_admin opc@${gse_admin} 'scp -o StrictHostKeyChecking=no -i ~/'${identity_domain}' ~/oracle_ebs_workshop.sh ~/root_ebs_workshop.sh ~/p22336899_R12_GENERIC.zip opc@'${target_ip}':~/'
+ssh -o StrictHostKeyChecking=no -i ssh_keys/gse_admin opc@${gse_admin} 'ssh -o StrictHostKeyChecking=no -i '${gse_admin_stagedir}'/'${identity_domain}' opc@'${target_ip}' "sudo mkdir -p '${gse_admin_stagedir}'"'
+ssh -o StrictHostKeyChecking=no -i ssh_keys/gse_admin opc@${gse_admin} 'scp -o StrictHostKeyChecking=no -i '${gse_admin_stagedir}'/'${identity_domain}' '${gse_admin_stagedir}'/oracle_ebs_workshop.sh '${gse_admin_stagedir}'/root_ebs_workshop.sh '${gse_admin_stagedir}'/p22336899_R12_GENERIC.zip opc@'${target_ip}':'${gse_admin_stagedir}
 echo "Running root commands..."
-ssh -o StrictHostKeyChecking=no -i ssh_keys/gse_admin opc@${gse_admin} 'ssh -o StrictHostKeyChecking=no -i ~/'${identity_domain}' opc@'${target_ip}' "sudo nohup sh ~/root_ebs_workshop.sh &"'
+ssh -o StrictHostKeyChecking=no -i ssh_keys/gse_admin opc@${gse_admin} 'ssh -o StrictHostKeyChecking=no -i '${gse_admin_stagedir}'/'${identity_domain}' opc@'${target_ip}' "sudo nohup sh '${gse_admin_stagedir}'/root_ebs_workshop.sh &"'
 echo "Running oracle commands..."
-ssh -o StrictHostKeyChecking=no -i ssh_keys/gse_admin opc@${gse_admin} 'ssh -o StrictHostKeyChecking=no -i ~/'${identity_domain}' opc@'${target_ip}' "sudo su - oracle -c '"'"'nohup sh ~/oracle_ebs_workshop.sh &'"'"'"'
+ssh -o StrictHostKeyChecking=no -i ssh_keys/gse_admin opc@${gse_admin} 'ssh -o StrictHostKeyChecking=no -i '${gse_admin_stagedir}'/'${identity_domain}' opc@'${target_ip}' "sudo su - oracle -c '"'"'nohup sh '${gse_admin_stagedir}'/oracle_ebs_workshop.sh &'"'"'"'
