f71fe9dffd31e666325ded204b601ccbbe9887c0
luisaria <luisaria>
2017-05-18 17:16:47 -0500

    create recipe

--- scripts/oracle_ebs_workshop.sh
+++ scripts/oracle_ebs_workshop.sh
@@ -1,44 +1,49 @@
 #oracle
 . /u01/install/APPS/EBSapps.env run
 
-expect -c "spawn /u01/install/APPS/fs1/inst/apps/EBSDB_ebssource/admin/scripts/adstpall.sh -skipNM -skipAdmin" #oracle
+expect -c "spawn /u01/install/APPS/fs1/inst/apps/EBSDB_ebssource/admin/scripts/adstpall.sh -skipNM -skipAdmin
 expect \"username:\"
 send \"apps\r\"
 expect \"APPS password:\"
 send \"apps\r\"
 expect \"Server password:\"
 send \"welcome1\r\"
+expect eof"
 
-expect -c "spawn perl /u01/install/APPS/fs1/EBSapps/appl/fnd/12.0.0/patch/115/bin/txkUpdateEBSDomain.pl -action=updateAdminPassword" #oracle
+expect -c "spawn perl /u01/install/APPS/fs1/EBSapps/appl/fnd/12.0.0/patch/115/bin/txkUpdateEBSDomain.pl -action=updateAdminPassword
 send \"Yes\r\"
 send \"\r\"
 send \"welcome1\r\"
 send \"welcome1\r\"
 send \"apps\r\"
+expect eof"
 
-expect -c "spawn /u01/install/APPS/fs1/inst/apps/EBSDB_ebssource/admin/scripts/adstrtal.sh"
+expect -c "spawn /u01/install/APPS/fs1/inst/apps/EBSDB_ebssource/admin/scripts/adstrtal.sh
 expect \"username:\"
 send \"apps\r\"
 expect \"APPS password:\"
 send \"apps\r\"
 expect \"Server password:\"
 send \"welcome1\r\"
+expect eof"
 
 . /u01/install/APPS/EBSapps.env run
 mkdir -p ~/logs
 cd ~/logs
 
-expect -c "spawn /u01/install/APPS/scripts/enableSYSADMIN.sh"
+expect -c "spawn /u01/install/APPS/scripts/enableSYSADMIN.sh
 expect \"new password for SYSADMIN:\"
 send \"welcome1\r\"
 expect \"enter password for SYSADMIN:\"
 send \"welcome1\r\"
+expect eof"
 
-expect -c "spawn /u01/install/APPS/scripts/enableDEMOusers.sh"
+expect -c "spawn /u01/install/APPS/scripts/enableDEMOusers.sh
 expect \"new password for SYSADMIN:\"
 send \"welcome1\r\"
 expect \"enter password for SYSADMIN:\"
 send \"welcome1\r\"
+expect eof"
 
 echo "ServerAliveInterval 100" >/home/oracle/.ssh/config
 ServerAliveInterval 100
