158a70fabab3ec57f2e92c5056e6911bc5c18b53
luisaria <luisaria>
2017-05-22 10:24:44 -0500

    adding workshop commands

--- scripts/oracle_ebs_workshop.sh
+++ scripts/oracle_ebs_workshop.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #oracle
 gse_admin_stagedir=$1
+identityDomain=$2
+targetIp=$3
 
 echo "ServerAliveInterval 100" > /home/oracle/.ssh/config
 
@@ -11,6 +13,20 @@ cd /u01
 [ ! -d /u01/dbstg ] && mkdir /u01/dbstg
 [ ! -d /u01/appstg ] && mkdir /u01/appstg
 
+expect -c "spawn ssh oracle@oc-141-144-144-58.compute.oraclecloud.com
+expect \"(yes/no)?\"
+send \"yes\r\"
+expect eof"
+
+expect -c "spawn ssh oracle@ebssource.compute-gse00010217.oraclecloud.internal
+expect \"(yes/no)?\"
+send \"yes\r\"
+expect eof"
+
+expect -c "spawn ssh oracle@ebssource
+expect \"(yes/no)?\"
+send \"yes\r\"
+expect eof"
 
 source /u01/install/APPS/12.1.0/EBSDB_ebssource.env
 sqlplus "/ as sysdba" <<'EOF'
