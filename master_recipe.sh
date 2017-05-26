80420b499c0b497ff040471fa394474a485a04a8
U-LUISARIA-LAP\luisaria <luisaria@LUISARIA-LAP.oradev.oraclecorp.com>
2017-05-25 19:23:11 -0500

    tools VM waiting

--- master_recipe.sh
+++ master_recipe.sh
@@ -10,13 +10,13 @@ executionPath=$4
 password=$5
 cd $executionPath
 if [ -f cache/$identity_domain/zone ] ; then
-	zone = `cat cache/$identity_domain/zone`
+	zone=`cat cache/$identity_domain/zone`
 else
 	mkdir -p cache/$identity_domain
 	echo zone > cache/$identity_domain/zone
 fi
 if [ -f cache/$identity_domain/datacenter ] ; then
-	datacenter = `cat cache/$identity_domain/datacenter`
+	datacenter=`cat cache/$identity_domain/datacenter`
 else
 	mkdir -p cache/$identity_domain
 	echo datacenter > cache/$identity_domain/datacenter
