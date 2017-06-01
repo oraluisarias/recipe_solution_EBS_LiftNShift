36e50213aa9f895bf0d93a96a62eb8ce343c65a3
U-LUISARIA-LAP\luisaria <luisaria@LUISARIA-LAP.oradev.oraclecorp.com>
2017-06-01 18:29:12 -0500

    app-deploy

--- RT/routes/index.js
+++ RT/routes/index.js
@@ -33,7 +33,7 @@ router.post('/getOPCZone', function(req, res, next) {
 	var cmd =  "python ../getOPCZone.py "+identity_domain+" "+password;
     console.log(cmd);
     exec(cmd, function (error2, stdout2, stderr2){      
-    	var result = JSON.parse( stdout2 );  
+    	var result = JSON.parse( stdout2.replace(/\'/g, '"') );  
         console.log( result );                           
 		res.json( result );                           
     });
