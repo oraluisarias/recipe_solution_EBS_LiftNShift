f81bc356caaa1ebae1476289a4ae0177d2946633
U-LUISARIA-LAP\luisaria <luisaria@LUISARIA-LAP.oradev.oraclecorp.com>
2017-06-02 15:25:09 -0500

    get multiple zones

--- RT/routes/index.js
+++ RT/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var exec = require('child_process').exec;
+var DMN = require("./models/cloudbots_domain_data");
 
 /* GET home page. */
 router.post('/install_marketplace_images', function(req, res, next) {
@@ -15,33 +16,38 @@ router.post('/install_marketplace_images', function(req, res, next) {
 	res.json( {"result":"Executing RT on background"} );                           
 });
 
-router.get('/getOPCZone/:identity_domain/:password', function(req, res, next) {
-	var identity_domain = req.params.identity_domain;
-	var password = req.params.password;
-	var cmd =  "python ../getOPCZone.py "+identity_domain+" "+password;
-    console.log(cmd);
-    exec(cmd, function (error2, stdout2, stderr2){      
-    	var result = JSON.parse( stdout2 );  
-        console.log( result );                           
-		res.json( result );                           
-    });
+router.get('/getOPCZone/:identity_domain/:password', function(req, res, next) {	
+	getOPCZone(req.params.identity_domain, req.params.password);
 });
 
-router.post('/getOPCZone', function(req, res, next) {
-	var identity_domain = req.body.identity_domain;
-	var password = req.body.password;
-	var cmd =  "python ../getOPCZone.py "+identity_domain+" "+password;
-    console.log(cmd);
-    exec(cmd, function (error2, stdout2, stderr2){      
-    	try{
-	    	var result = JSON.parse( stdout2.replace(/\'/g, '"') );  
-	        console.log( result );                           
-			res.json( result );       
-		} catch (e) {
-			res.send( stdout2 );       
-	      	console.error(e.stack);
-	    }                    
-    });
+router.post('/getOPCZone', function(req, res, next) {	
+	getOPCZone(req.body.identity_domain, req.body.password);
 });
+
+function getOPCZone(identity_domain, password){
+	DMN.findOne({ where: { "identity_domain" : identity_domain } }).then(function(domain) {
+        if(domain){
+			res.json( { domain.identity_domain : domain.domain_data } );   
+        }
+        else{
+			var cmd =  "python ../getOPCZone.py "+identity_domain+" "+password;
+		    console.log(cmd);
+		    exec(cmd, function (error2, stdout2, stderr2){      
+		    	try{
+			    	var result = JSON.parse( stdout2.replace(/\'/g, '"') );  
+			        console.log( result );                           
+					DMN.build( { identity_domain : identity_domain, domain_data : result[identity_domain] } ).save().then(function() {
+						res.json( result );      				    	
+				  	}).catch(function (err) {
+		                console.log(err);
+		            }); 
+				} catch (e) {
+					res.send( stdout2 );       
+			      	console.error(e.stack);
+			    }                    
+		    });
+        }
+    });
+}
 
 module.exports = router;
