9a7e184b9a9aefac5528f19e1014a34d5d32482d
U-LUISARIA-LAP\luisaria <luisaria@LUISARIA-LAP.oradev.oraclecorp.com>
2017-06-02 10:13:27 -0500

    enable DEMO users and start web tier in source VM

--- scripts/oracle_ebs_workshop.sh
+++ scripts/oracle_ebs_workshop.sh
@@ -3,6 +3,7 @@
 gse_admin_stagedir=$1
 identityDomain=$2
 targetIp=$3
+host_name="oc-"`echo "$targetIp" | tr . -`
 
 echo "ServerAliveInterval 100" > /home/oracle/.ssh/config
 
@@ -13,32 +14,67 @@ cd /u01
 [ ! -d /u01/dbstg ] && mkdir /u01/dbstg
 [ ! -d /u01/appstg ] && mkdir /u01/appstg
 
-expect -c "spawn ssh oracle@oc-141-144-144-58.compute.oraclecloud.com
-expect \"(yes/no)?\"
-send \"yes\r\"
-expect eof"
+# expect -c "spawn ssh oracle@oc-141-144-144-58.compute.oraclecloud.com
+# expect \"(yes/no)?\"
+# send \"yes\r\"
+# expect eof"
 
-expect -c "spawn ssh oracle@ebssource.compute-gse00010217.oraclecloud.internal
-expect \"(yes/no)?\"
-send \"yes\r\"
-expect eof"
+# expect -c "spawn ssh oracle@ebssource.compute-gse00010217.oraclecloud.internal
+# expect \"(yes/no)?\"
+# send \"yes\r\"
+# expect eof"
 
-expect -c "spawn ssh oracle@ebssource
-expect \"(yes/no)?\"
-send \"yes\r\"
-expect eof"
+# expect -c "spawn ssh oracle@ebssource
+# expect \"(yes/no)?\"
+# send \"yes\r\"
+# expect eof"
 
-source /u01/install/APPS/12.1.0/EBSDB_ebssource.env
-sqlplus "/ as sysdba" <<'EOF'
-select log_mode from v$database;
-shutdown immediate
-startup mount;
-alter database archivelog;
-alter database open;
-select open_mode from v$database;
-exit;
-EOF
+# source /u01/install/APPS/12.1.0/EBSDB_ebssource.env
+# sqlplus "/ as sysdba" <<'EOF'
+# select log_mode from v$database;
+# shutdown immediate
+# startup mount;
+# alter database archivelog;
+# alter database open;
+# select open_mode from v$database;
+# exit;
+# EOF
 
 . /u01/install/APPS/EBSapps.env run
 cd $gse_admin_stagedir/RemoteClone_v1.7
-perl ebsclone.pl
+# perl ebsclone.pl
+cd $ADMIN_SCRIPTS_HOME
+sh adstpall.sh
+
+expect -c "spawn sh configwebentry.sh
+expect \"(https/http):?\"
+send \"http\r\"
+expect \"public):?\"
+send \"${host_name}\r\"
+expect \"domain.com):?\"
+send \"compute.oraclecloud.com\r\"
+expect \"443/80):?\"
+send \"8000\r\"
+expect \" /u01/install/APPS):?\"
+send \"/u01/install/APPS\r\"
+send \"\r\"
+expect eof"
+
+expect -c "spawn sh adstrtal.sh
+expect \"APPS username:?\"
+send \"apps\r\"
+expect \"APPS password:?\"
+send \"apps\r\"
+expect \"WebLogic Server password:?\"
+send \"welcome1\r\"
+send \"\r\"
+expect eof"
+
+expect -c "spawn sh /u01/install/APPS/scripts/enableDEMOusers.sh
+expect \"Enter new password for DEMO users:?\"
+send \"welcome1\r\"
+expect \"Re-enter password for DEMO users:?\"
+send \"welcome1\r\"
+send \"\r\"
+expect eof"
+
