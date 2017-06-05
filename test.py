b57ae7b9b5b352456fcdced2c71e1314fb2422af
U-LUISARIA-LAP\luisaria <luisaria@LUISARIA-LAP.oradev.oraclecorp.com>
2017-06-05 18:14:28 -0500

    get multiple zones

--- test.py
+++ test.py
@@ -6,5 +6,6 @@ domains = json.loads(content)
 print ("Reading domains...", domains)
 for domain in domains:	
 	print ( domain )
-	cloud_password = demo_central.getDCEnvironment("metcs-" + domain)["items"][0]["password"]
+	try: cloud_password = demo_central.getDCEnvironment("metcs-" + domain)["items"][0]["password"]
+	except: continue	
 	print ( requests.post("http://gse-admin.oraclecloud.com:7002/getOPCZone", data={ "identity_domain":domain, "password":cloud_password } ).text )
