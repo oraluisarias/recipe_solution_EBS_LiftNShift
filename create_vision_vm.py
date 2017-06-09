057c76e6c678fc5a996afc5bcdb84d3d2d6a4bdf
U-LUISARIA-LAP\luisaria <luisaria@LUISARIA-LAP.oradev.oraclecorp.com>
2017-06-08 21:02:30 -0500

    wait for volume

--- create_vision_vm.py
+++ create_vision_vm.py
@@ -41,18 +41,20 @@ while real_source_instance_name != "" and real_source_volume_name != "" :
 	volumes = opcc.getVolumes( cloud_username )
 	print ("Waiting for instance and volume to be created, sleeping 1 minute per iteration ",str(time_ellapsed)," minutes passed...")
 	try:
-		for volume in volumes["result"]: 
-			if volume['name'].find(source_volume_name) > 0:		
-				print ("Volume state: ", volume["status"])
-				if volume["status"] == "Online":
-					real_source_volume_name = volume['name']					
-		for instance in instances["result"]: 
-			if instance['name'].find(source_instance_name) > 0:							
-				print ("Instance state: ", instance["state"])
-				if instance["state"] == "running":
-					source_public_ip = opcc.getReservedIP(cloud_username, instance["vcable_id"])
-					real_source_instance_name = instance['name']
-					print ("Retrieving IP and real name of the newly created service...", source_public_ip, real_source_instance_name)
+		if real_source_volume_name == "":				
+			for volume in volumes["result"]: 
+				if volume['name'].find(source_volume_name) > 0:		
+					print ("Volume state: ", volume["status"])
+					if volume["status"] == "Online":
+						real_source_volume_name = volume['name']	
+		if real_source_instance_name == "":				
+			for instance in instances["result"]: 
+				if instance['name'].find(source_instance_name) > 0:							
+					print ("Instance state: ", instance["state"])
+					if instance["state"] == "running":
+						source_public_ip = opcc.getReservedIP(cloud_username, instance["vcable_id"])
+						real_source_instance_name = instance['name']
+						print ("Retrieving IP and real name of the newly created service...", source_public_ip, real_source_instance_name)
 		if real_source_instance_name == "" :
 			time.sleep(60)
 			time_ellapsed=time_ellapsed+1	  
