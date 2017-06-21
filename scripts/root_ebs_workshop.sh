edc3a993acf1429e4f4b2a49254fb9e4e6ac29a8
U-LUISARIA-LAP\luisaria <luisaria@LUISARIA-LAP.oradev.oraclecorp.com>
2017-06-21 17:48:27 -0500

    root commands

--- scripts/root_ebs_workshop.sh
+++ scripts/root_ebs_workshop.sh
@@ -8,16 +8,16 @@ yum install -y curl tar gcc gcc-c++ openssl-devel wget xz make tcl expect dos2un
 cp /home/opc/.ssh/authorized_keys /home/oracle/.ssh/authorized_keys #root
 chown oracle:oinstall /home/oracle/.ssh/authorized_keys #root
 
-chmod 777 '${gse_admin_stagedir}'/oracle_ebs_workshop.sh
-dos2unix '${gse_admin_stagedir}'/change_fqdn.sh
-sh '${gse_admin_stagedir}'/change_fqdn.sh '${ipHOST}'.compute.oraclecloud.com
-unzip -o '${gse_admin_stagedir}'/p22336899_R12_GENERIC.zip
-chown oracle:dba -R '${gse_admin_stagedir}'
-chmod 777 -R '${gse_admin_stagedir}'
-cp -rf '${gse_admin_stagedir}'/RemoteClone_v1.7/cln.props '${gse_admin_stagedir}'/RemoteClone_v1.7/cln.props.ORIG
-cp -rf '${gse_admin_stagedir}'/cln.props '${gse_admin_stagedir}'/RemoteClone_v1.7/cln.props
+chmod 777 $gse_admin_stagedir/oracle_ebs_workshop.sh
+dos2unix $gse_admin_stagedir/change_fqdn.sh
+sh $gse_admin_stagedir/change_fqdn.sh "$ipHOST.compute.oraclecloud.com"
+unzip -o $gse_admin_stagedir/p22336899_R12_GENERIC.zip
+chown oracle:dba -R $gse_admin_stagedir
+chmod 777 -R $gse_admin_stagedir
+cp -rf $gse_admin_stagedir/RemoteClone_v1.7/cln.props $gse_admin_stagedir/RemoteClone_v1.7/cln.props.ORIG
+cp -rf $gse_admin_stagedir/cln.props $gse_admin_stagedir/RemoteClone_v1.7/cln.props
 
-if [ -f /u01/install/APPS/apps-unlimited-ebs/ProvisionEBS.xml && -f '${gse_admin_stagedir}'/ProvisionEBS.xml ] ; then
+if [ -f /u01/install/APPS/apps-unlimited-ebs/ProvisionEBS.xml ] && [ -f $gse_admin_stagedir/ProvisionEBS.xml ] ; then
 	mv /u01/install/APPS/apps-unlimited-ebs/ProvisionEBS.xml /u01/install/APPS/apps-unlimited-ebs/ProvisionEBS.xml.ORIG 
-	cp '${gse_admin_stagedir}'/ProvisionEBS.xml /u01/install/APPS/apps-unlimited-ebs/ProvisionEBS.xml
+	cp $gse_admin_stagedir/ProvisionEBS.xml /u01/install/APPS/apps-unlimited-ebs/ProvisionEBS.xml
 fi
