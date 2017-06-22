a31c218ea1fa91024457213d34c66416befc46b9
U-LUISARIA-LAP\luisaria <luisaria@LUISARIA-LAP.oradev.oraclecorp.com>
2017-06-21 23:36:28 -0500

    lost access to OPC

--- scripts/root_ebs_workshop.sh
+++ scripts/root_ebs_workshop.sh
@@ -3,14 +3,14 @@ identity_domain=$1
 ipHOST=$2
 gse_admin_stagedir="/app/EBS_LiftNShift/"${identity_domain}
 
+dos2unix $gse_admin_stagedir/change_fqdn.sh
+sh $gse_admin_stagedir/change_fqdn.sh "$ipHOST.compute.oraclecloud.com"
 yum update -y #root
 yum install -y curl tar gcc gcc-c++ openssl-devel wget xz make tcl expect dos2unix #root
 cp /home/opc/.ssh/authorized_keys /home/oracle/.ssh/authorized_keys #root
 chown oracle:oinstall /home/oracle/.ssh/authorized_keys #root
 
 chmod 777 $gse_admin_stagedir/oracle_ebs_workshop.sh
-dos2unix $gse_admin_stagedir/change_fqdn.sh
-sh $gse_admin_stagedir/change_fqdn.sh "$ipHOST.compute.oraclecloud.com"
 unzip -o $gse_admin_stagedir/p22336899_R12_GENERIC.zip
 chown oracle:dba -R $gse_admin_stagedir
 chmod 777 -R $gse_admin_stagedir
