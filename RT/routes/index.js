e3279f16749d12400c957a64e839b06fd0836329
U-LUISARIA-LAP\luisaria <luisaria@LUISARIA-LAP.oradev.oraclecorp.com>
2017-07-10 16:37:38 -0500

    rest endpoint

--- RT/routes/index.js
+++ RT/routes/index.js
@@ -4,6 +4,19 @@ var exec = require('child_process').exec;
 var DMN = require("../models/cloudbots_domain_data");
 
 /* GET home page. */
+router.post('/wassy', function(req, res, next) {
+	var identity_domain = req.body.identity_domain;
+	var datacenter = req.body.datacenter;
+	var password = req.body.password;
+	var cmd =  "python ../wassy.py "+identity_domain+" "+password;
+    console.log(cmd);
+    exec(cmd, {maxBuffer: 1024 * 1000 * 1000}, function (error2, stdout2, stderr2){        
+        console.log( stdout2 );                           
+		res.json( {"result":stdout2} );                           
+    });
+});
+
+/* GET home page. */
 router.post('/install_marketplace_images', function(req, res, next) {
 	var identity_domain = req.body.identity_domain;
 	var datacenter = req.body.datacenter;
