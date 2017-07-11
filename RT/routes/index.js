b1082c3b7a660993a82a836a214f86a47b1b8927
U-LUISARIA-LAP\luisaria <luisaria@LUISARIA-LAP.oradev.oraclecorp.com>
2017-07-11 15:18:12 -0500

    geckodriver

--- RT/routes/index.js
+++ RT/routes/index.js
@@ -55,8 +55,8 @@ function getOPCZone(identity_domain, password, username, res){
 		    exec(cmd, function (error2, stdout2, stderr2){      
 		    	try{
 		    		console.log(stdout2);
-			    	// var result = JSON.parse( stdout2.replace(/\'/g, '"') );  
-			    	var result = stdout2;  
+			    	// var result = stdout2;  
+			    	var result = JSON.parse( stdout2.replace(/\n/g, '').replace(/\'/g, '"') );  
 			        console.log( result );                           
 					DMN.build( { 
 						identity_domain : identity_domain, 
